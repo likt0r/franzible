d9ff4f302418f563790afd63c73d55ed9b77f593
bewr <benjamin.werner@mfn.berlin>
2021-06-27 00:21:43 +0200

    adds error handling and media reloding after error

--- frontend/store/player.js
+++ frontend/store/player.js
@@ -3,268 +3,269 @@ import { PLAYER_STATE_ENUM, getInstance, init } from '../tools/AudioPlayer'
 import { getFullUrl } from '../tools/url'
 
 export const state = () => ({
-  bookId: null,
-  book: null,
-  fileIndex: null,
-  filePosition: 0,
-  playerState: PLAYER_STATE_ENUM.stopped,
-  fastStep: 30,
-  progressId: null,
-  speed: 100,
+	bookId: null,
+	book: null,
+	fileIndex: null,
+	filePosition: 0,
+	playerState: PLAYER_STATE_ENUM.stopped,
+	fastStep: 30,
+	progressId: null,
+	speed: 100,
 })
 
 export const mutations = {
-  SET_BOOK_FILE(
-    state,
-    { bookId, book, fileIndex, filePosition = 0, progressId }
-  ) {
-    state.bookId = bookId
-    state.fileIndex = fileIndex
-    state.book = book
-    state.filePosition = filePosition
-    state.progressId = progressId
-  },
-  SET_FILE(state, { fileIndex, filePosition = 0 }) {
-    state.fileIndex = fileIndex
-    state.filePosition = filePosition
-  },
-  // playerState = playing | stopped | loading | error
-  SET_STATE(state, playerState) {
-    if (Object.values(PLAYER_STATE_ENUM).includes(playerState)) {
-      state.playerState = playerState
-    } else {
-      throw new Error(`Unkown Player state: ${playerState}`)
-    }
-  },
+	SET_BOOK_FILE(
+		state,
+		{ bookId, book, fileIndex, filePosition = 0, progressId }
+	) {
+		state.bookId = bookId
+		state.fileIndex = fileIndex
+		state.book = book
+		state.filePosition = filePosition
+		state.progressId = progressId
+	},
+	SET_FILE(state, { fileIndex, filePosition = 0 }) {
+		state.fileIndex = fileIndex
+		state.filePosition = filePosition
+	},
+	// playerState = playing | stopped | loading | error
+	SET_STATE(state, playerState) {
+		if (Object.values(PLAYER_STATE_ENUM).includes(playerState)) {
+			console.log('#Player state is: ', playerState)
+			state.playerState = playerState
+		} else {
+			throw new Error(`Unkown Player state: ${playerState}`)
+		}
+	},
 
-  SET_FILE_POSTION(state, filePosition) {
-    state.filePosition = filePosition
-  },
-  SET_SPEED(state, speed) {
-    state.speed = speed
-  },
+	SET_FILE_POSTION(state, filePosition) {
+		state.filePosition = filePosition
+	},
+	SET_SPEED(state, speed) {
+		state.speed = speed
+	},
 }
 
 export const actions = {
-  loadFile(
-    { dispatch, commit, getters, state, rootGetters },
-    { bookId, fileIndex, filePosition = 0, startPlaying = false }
-  ) {
-    if (state.bookId !== bookId || state.fileIndex !== fileIndex) {
-      const book = rootGetters['books/get'](bookId)
-      if (!book) {
-        throw new Error('Book not found with id', bookId)
-      }
-      const { _id: progressId } = rootGetters['progress/find']({
-        query: {
-          bookId,
-        },
-      }).data[0]
+	loadFile(
+		{ dispatch, commit, getters, state, rootGetters },
+		{ bookId, fileIndex, filePosition = 0, startPlaying = false }
+	) {
+		if (state.bookId !== bookId || state.fileIndex !== fileIndex) {
+			const book = rootGetters['books/get'](bookId)
+			if (!book) {
+				throw new Error('Book not found with id', bookId)
+			}
+			const { _id: progressId } = rootGetters['progress/find']({
+				query: {
+					bookId,
+				},
+			}).data[0]
 
-      commit('SET_BOOK_FILE', {
-        bookId,
-        book,
-        fileIndex,
-        filePosition,
-        progressId,
-      })
-      getInstance().loadAudioBook({
-        audioUrl: getFullUrl(book.files[fileIndex].filepath),
-        audioDbId: book.files[fileIndex].dbId,
-        filePosition,
-        startPlaying,
-      })
-      // set media Notification from Chrome
-      if ('mediaSession' in navigator) {
-        /** */
-        navigator.mediaSession.metadata = new MediaMetadata({
-          title: state.book.title,
-          artist: `Kapitel ${fileIndex + 1}/${book.files.length}`,
-          // album:
-          artwork: [
-            // {
-            //   src: 'https://dummyimage.com/96x96',
-            //   sizes: '96x96',
-            //   type: 'image/png',
-            // },
-            // {
-            //   src: 'https://dummyimage.com/128x128',
-            //   sizes: '128x128',
-            //   type: 'image/png',
-            // },
-            // {
-            //   src: 'https://dummyimage.com/192x192',
-            //   sizes: '192x192',
-            //   type: 'image/png',
-            // },
-            // {
-            //   src: 'https://dummyimage.com/256x256',
-            //   sizes: '256x256',
-            //   type: 'image/png',
-            // },
-            // {
-            //   src: 'https://dummyimage.com/384x384',
-            //   sizes: '384x384',
-            //   type: 'image/png',
-            // },
-            {
-              src: getFullUrl(state.book.cover[0] || '/logo.png'),
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-        })
+			commit('SET_BOOK_FILE', {
+				bookId,
+				book,
+				fileIndex,
+				filePosition,
+				progressId,
+			})
+			getInstance().loadAudioBook({
+				audioUrl: getFullUrl(book.files[fileIndex].filepath),
+				audioDbId: book.files[fileIndex].dbId,
+				filePosition,
+				startPlaying,
+			})
+			// set media Notification from Chrome
+			if ('mediaSession' in navigator) {
+				/** */
+				navigator.mediaSession.metadata = new MediaMetadata({
+					title: state.book.title,
+					artist: `Kapitel ${fileIndex + 1}/${book.files.length}`,
+					// album:
+					artwork: [
+						// {
+						//   src: 'https://dummyimage.com/96x96',
+						//   sizes: '96x96',
+						//   type: 'image/png',
+						// },
+						// {
+						//   src: 'https://dummyimage.com/128x128',
+						//   sizes: '128x128',
+						//   type: 'image/png',
+						// },
+						// {
+						//   src: 'https://dummyimage.com/192x192',
+						//   sizes: '192x192',
+						//   type: 'image/png',
+						// },
+						// {
+						//   src: 'https://dummyimage.com/256x256',
+						//   sizes: '256x256',
+						//   type: 'image/png',
+						// },
+						// {
+						//   src: 'https://dummyimage.com/384x384',
+						//   sizes: '384x384',
+						//   type: 'image/png',
+						// },
+						{
+							src: getFullUrl(state.book.cover[0] || '/logo.png'),
+							sizes: '512x512',
+							type: 'image/png',
+						},
+					],
+				})
 
-        navigator.mediaSession.setActionHandler('play', function () {
-          dispatch('resume')
-        })
-        navigator.mediaSession.setActionHandler('pause', function () {
-          dispatch('pause')
-        })
-        navigator.mediaSession.setActionHandler('seekbackward', function () {
-          dispatch('fastRewind')
-        })
-        // navigator.mediaSession.setActionHandler('seekforward', function () {})
-        // navigator.mediaSession.setActionHandler('previoustrack', function () {})
-        // navigator.mediaSession.setActionHandler('nexttrack', function () {})
-      }
-    } else {
-      console.warn('ACTION/playFile: Boock and File are the same as current')
-    }
-  },
-  setSpeed({ commit }, speed) {
-    commit('SET_SPEED', speed)
-    getInstance().setSpeed(speed / 100)
-  },
-  pause() {
-    getInstance().pause()
-  },
+				navigator.mediaSession.setActionHandler('play', function () {
+					dispatch('resume')
+				})
+				navigator.mediaSession.setActionHandler('pause', function () {
+					dispatch('pause')
+				})
+				navigator.mediaSession.setActionHandler('seekbackward', function () {
+					dispatch('fastRewind')
+				})
+				// navigator.mediaSession.setActionHandler('seekforward', function () {})
+				// navigator.mediaSession.setActionHandler('previoustrack', function () {})
+				// navigator.mediaSession.setActionHandler('nexttrack', function () {})
+			}
+		} else {
+			console.warn('ACTION/playFile: Boock and File are the same as current')
+		}
+	},
+	setSpeed({ commit }, speed) {
+		commit('SET_SPEED', speed)
+		getInstance().setSpeed(speed / 100)
+	},
+	pause() {
+		getInstance().pause()
+	},
 
-  resume() {
-    getInstance().resume()
-  },
+	resume() {
+		getInstance().resume()
+	},
 
-  fastForward({ state, dispatch }) {
-    const file = state.book.files[state.fileIndex]
-    if (state.filePosition + state.fastStep < file.duration) {
-      getInstance().seek(state.filePosition + state.fastStep)
-    } else {
-      dispatch('skipNext', state.filePosition + state.fastStep - file.duration)
-    }
-  },
+	fastForward({ state, dispatch }) {
+		const file = state.book.files[state.fileIndex]
+		if (state.filePosition + state.fastStep < file.duration) {
+			getInstance().seek(state.filePosition + state.fastStep)
+		} else {
+			dispatch('skipNext', state.filePosition + state.fastStep - file.duration)
+		}
+	},
 
-  fastRewind({ state, dispatch }) {
-    if (state.filePosition - state.fastStep > 0) {
-      getInstance().seek(state.filePosition - state.fastStep)
-    } else if (state.fileIndex - 1 > -1) {
-      dispatch(
-        'skipPrevious',
-        state.book.files[state.fileIndex - 1].duration +
-          (state.filePosition - state.fastStep)
-      )
-    } else {
-      getInstance().seek(0)
-    }
-  },
+	fastRewind({ state, dispatch }) {
+		if (state.filePosition - state.fastStep > 0) {
+			getInstance().seek(state.filePosition - state.fastStep)
+		} else if (state.fileIndex - 1 > -1) {
+			dispatch(
+				'skipPrevious',
+				state.book.files[state.fileIndex - 1].duration +
+					(state.filePosition - state.fastStep)
+			)
+		} else {
+			getInstance().seek(0)
+		}
+	},
 
-  seek(_, position) {
-    getInstance().seek(position)
-  },
+	seek(_, position) {
+		getInstance().seek(position)
+	},
 
-  skipNext({ commit, state }, position = 0) {
-    // has next
-    if (state.fileIndex < state.book.files.length - 1) {
-      commit('SET_FILE', {
-        fileIndex: state.fileIndex + 1,
-        filePosition: position,
-      })
-      getInstance().loadAudioBook({
-        audioUrl: getFullUrl(state.book.files[state.fileIndex].filepath),
-        audioDbId: state.book.files[state.fileIndex].dbId,
-        filePosition: position,
-        startPlaying: true,
-      })
-    } else {
-      getInstance().seek(state.book.files[state.fileIndex].duration - 0.01)
-    }
-  },
+	skipNext({ commit, state }, position = 0) {
+		// has next
+		if (state.fileIndex < state.book.files.length - 1) {
+			commit('SET_FILE', {
+				fileIndex: state.fileIndex + 1,
+				filePosition: position,
+			})
+			getInstance().loadAudioBook({
+				audioUrl: getFullUrl(state.book.files[state.fileIndex].filepath),
+				audioDbId: state.book.files[state.fileIndex].dbId,
+				filePosition: position,
+				startPlaying: true,
+			})
+		} else {
+			getInstance().seek(state.book.files[state.fileIndex].duration - 0.01)
+		}
+	},
 
-  skipPrevious({ commit, state }, position = 0) {
-    // has next
-    if (state.fileIndex > 0) {
-      commit('SET_FILE', { fileIndex: state.fileIndex - 1, filePosition: 0 })
-      getInstance().loadAudioBook({
-        audioUrl: getFullUrl(state.book.files[state.fileIndex].filepath),
-        audioDbId: state.book.files[state.fileIndex].dbId,
-        filePosition: position,
-        startPlaying: true,
-      })
-    } else {
-      getInstance().seek(0)
-    }
-  },
+	skipPrevious({ commit, state }, position = 0) {
+		// has next
+		if (state.fileIndex > 0) {
+			commit('SET_FILE', { fileIndex: state.fileIndex - 1, filePosition: 0 })
+			getInstance().loadAudioBook({
+				audioUrl: getFullUrl(state.book.files[state.fileIndex].filepath),
+				audioDbId: state.book.files[state.fileIndex].dbId,
+				filePosition: position,
+				startPlaying: true,
+			})
+		} else {
+			getInstance().seek(0)
+		}
+	},
 }
 
 export const getters = {
-  speed(state) {
-    return state.speed
-  },
-  activeBookId(state) {
-    return state.bookId
-  },
-  activeFileIndex(state) {
-    return state.fileIndex
-  },
-  isPlaying(state) {
-    return state.playerState === PLAYER_STATE_ENUM.playing
-  },
-  isLoading(state) {
-    return state.playerState === PLAYER_STATE_ENUM.loading
-  },
-  filePositionInSecs(state) {
-    return state.filePosition
-  },
-  fileRemainingTime(state) {
-    return state.book
-      ? state.book.files[state.fileIndex].duration - state.filePosition
-      : 0
-  },
-  fileDuration(state) {
-    return state.book ? state.book.files[state.fileIndex].duration : 0
-  },
-  hasPrevious(state) {
-    return state.fileIndex > 0
-  },
-  hasNext(state) {
-    return state.fileIndex < state.files.length - 1
-  },
+	speed(state) {
+		return state.speed
+	},
+	activeBookId(state) {
+		return state.bookId
+	},
+	activeFileIndex(state) {
+		return state.fileIndex
+	},
+	isPlaying(state) {
+		return state.playerState === PLAYER_STATE_ENUM.playing
+	},
+	isLoading(state) {
+		return state.playerState === PLAYER_STATE_ENUM.loading
+	},
+	filePositionInSecs(state) {
+		return state.filePosition
+	},
+	fileRemainingTime(state) {
+		return state.book
+			? state.book.files[state.fileIndex].duration - state.filePosition
+			: 0
+	},
+	fileDuration(state) {
+		return state.book ? state.book.files[state.fileIndex].duration : 0
+	},
+	hasPrevious(state) {
+		return state.fileIndex > 0
+	},
+	hasNext(state) {
+		return state.fileIndex < state.files.length - 1
+	},
 }
 
 export const playerInitPlugin = (store) => {
-  // called when the store is initialized
-  console.log('#player: Init Player')
-  if (process.client) {
-    init(store)
-  }
-  store.subscribe((mutation, state) => {
-    // called after every mutation.
-    // The mutation comes in the format of `{ type, payload }`.
-    if (
-      [
-        'player/SET_BOOK_FILE',
-        'player/SET_FILE',
-        'player/SET_FILE_POSTION',
-      ].includes(mutation.type)
-    ) {
-      console.log('update position')
-      store.dispatch('progress/patch', [
-        state.player.progressId,
-        {
-          fileIndex: state.player.fileIndex,
-          filePosition: state.player.filePosition,
-          played: true,
-        },
-      ])
-    }
-  })
+	// called when the store is initialized
+	console.log('#player: Init Player')
+	if (process.client) {
+		init(store)
+	}
+	store.subscribe((mutation, state) => {
+		// called after every mutation.
+		// The mutation comes in the format of `{ type, payload }`.
+		if (
+			[
+				'player/SET_BOOK_FILE',
+				'player/SET_FILE',
+				'player/SET_FILE_POSTION',
+			].includes(mutation.type)
+		) {
+			console.log('update position')
+			store.dispatch('progress/patch', [
+				state.player.progressId,
+				{
+					fileIndex: state.player.fileIndex,
+					filePosition: state.player.filePosition,
+					played: true,
+				},
+			])
+		}
+	})
 }
--- frontend/tools/AudioPlayer.js
+++ frontend/tools/AudioPlayer.js
@@ -1,102 +1,133 @@
 import { getDatabase } from './database'
 const database = getDatabase()
 export const PLAYER_STATE_ENUM = {
-  playing: 'playing',
-  stopped: 'stopped',
-  loading: 'loading',
-  error: 'error',
+	playing: 'playing',
+	stopped: 'stopped',
+	loading: 'loading',
+	error: 'error',
 }
 
 class AudioPlayer {
-  constructor(store) {
-    this.store = null
-    this.elAudio = document.createElement('AUDIO')
-    this.elPreloader = document.createElement('AUDIO')
-    this.playing = false
-    this.loading = false
-    this.fileIndex = null
-    this.files = []
-    this.fileIndex = 0
-    this.filePosition = 0
-    this.store = store
+	constructor(store) {
+		this.reloadSrcFlag = false
+		this.store = null
+		this.elAudio = document.createElement('AUDIO')
+		this.elPreloader = document.createElement('AUDIO')
+		this.playing = false
+		this.loading = false
+		this.fileIndex = null
+		this.files = []
+		this.fileIndex = 0
+		this.filePosition = 0
+		this.store = store
+		this.currentSrc = ''
 
-    // Add all Media Listeners
-    this.elAudio.addEventListener('play', () => {
-      // Fires when the audio/video has been started or is no longer paused
-      console.log('#player: play', this.fileIndex)
-      this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.playing)
-    })
-    this.elAudio.addEventListener('playing', () => {
-      // Fires when the audio/video is playing after having
-      // been paused or stopped for buffering
-      console.log('#player: playing', this.fileIndex)
-      this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.playing)
-    })
-    this.elAudio.addEventListener('pause', () => {
-      // Fires when the audio/video has been paused
-      console.log('#player: pause', this.fileIndex)
-      this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.stopped)
-    })
-    this.elAudio.addEventListener('error', () => {
-      // Fires when the audio/video has been paused
-      console.log('#player: error', this.fileIndex)
-      this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.error)
-    })
-    this.elAudio.addEventListener('loadstart', () => {
-      // Fires when the audio/video has been paused
-      console.log('#player: loadstart', this.fileIndex)
-      this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.loading)
-    })
-    this.elAudio.addEventListener('timeupdate', (event) => {
-      // Fires when the audio/video has been paused
-      // console.log('#player: timeupdate', event)
-      this.store.commit('player/SET_FILE_POSTION', this.elAudio.currentTime)
-    })
-    this.elAudio.addEventListener('ended', (event) => {
-      // Fires when the audio/video has been paused
-      // console.log('#player: timeupdate', event)
-      this.store.dispatch('player/skipNext')
-    })
-  }
+		// Add all Media Listeners
+		this.elAudio.addEventListener('play', () => {
+			// Fires when the audio/video has been started or is no longer paused
+			console.log('#player: play', this.fileIndex)
+			this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.playing)
+		})
+		this.elAudio.addEventListener('playing', () => {
+			// Fires when the audio/video is playing after having
+			// been paused or stopped for buffering
+			console.log('#player: playing', this.fileIndex)
+			this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.playing)
+		})
+		this.elAudio.addEventListener('pause', () => {
+			// Fires when the audio/video has been paused
+			console.log('#player: pause', this.fileIndex)
+			this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.stopped)
+		})
+		this.elAudio.addEventListener('error', (error) => {
+			// Fires when the audio/video has been paused
+			console.log('#player: error', error)
+			this.reloadSrcFlag = true
+			this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.error)
+		})
+		this.elAudio.addEventListener('stalled', () => {
+			// Fires when the audio/video has been paused
+			console.log('#player: stalled', this.fileIndex)
+			this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.error)
+		})
+		this.elAudio.addEventListener('suspend', () => {
+			// Fires when the audio/video has been paused
+			console.log('#player: suspend', this.fileIndex)
+		})
+		this.elAudio.addEventListener('loadstart', () => {
+			// Fires when the audio/video has been paused
+			console.log('#player: loadstart', this.fileIndex)
+			this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.loading)
+		})
+		this.elAudio.addEventListener('timeupdate', (event) => {
+			// Fires when the audio/video has been paused
+			// console.log('#player: timeupdate', event)
+			this.store.commit('player/SET_FILE_POSTION', this.elAudio.currentTime)
+		})
+		this.elAudio.addEventListener('ended', (event) => {
+			// Fires when the audio/video has been paused
+			// console.log('#player: timeupdate', event)
+			this.store.dispatch('player/skipNext')
+		})
+	}
 
-  async loadAudioBook({ audioUrl, audioDbId, filePosition = 0, startPlaying }) {
-    if (audioDbId) {
-      if (this.elAudio.src && this.elAudio.src.startsWith('blob:')) {
-        URL.revokeObjectURL(this.elAudio.src)
-      }
-      this.elAudio.src = await database.getFileContentUrl(audioDbId)
-    } else {
-      this.elAudio.src = audioUrl
-    }
-    this.elAudio.currentTime = filePosition
-    if (startPlaying) {
-      this.elAudio.play()
-    }
-  }
+	async loadAudioBook({ audioUrl, audioDbId, filePosition = 0, startPlaying }) {
+		if (audioDbId) {
+			if (this.elAudio.src && this.elAudio.src.startsWith('blob:')) {
+				URL.revokeObjectURL(this.elAudio.src)
+			}
+			this.elAudio.src = await database.getFileContentUrl(audioDbId)
+		} else {
+			this.elAudio.src = audioUrl
+		}
+		this.currentSrc = this.elAudio.src
+		this.elAudio.currentTime = filePosition
+		if (startPlaying) {
+			this.__play()
+		}
+	}
 
-  pause() {
-    this.elAudio.pause()
-  }
+	__play() {
+		// If error occurs src has to be reloaded to prevent media stall
+		if (this.reloadSrcFlag) {
+			this.reloadSrcFlag = false
+			this.elAudio.load()
+		}
+		this.elAudio.play().catch((error) => {
+			if (error.message === 'The element has no supported sources.') {
+				// update src to prevent stall on this file
+				// this.elAudio.src = `${this.currentSrc}#${Date.now()}`
+				// setTimeout(() => {
+				// 	this.elAudio.src = `${this.currentSrc}#${Date.now()}`
+				// }, 0)
+			}
+			// this.store.commit('player/SET_STATE', PLAYER_STATE_ENUM.error)
+		})
+	}
 
-  resume() {
-    this.elAudio.play()
-  }
+	pause() {
+		this.elAudio.pause()
+	}
 
-  setSpeed(speed) {
-    this.elAudio.playbackRate = speed
-  }
+	resume() {
+		this.__play()
+	}
 
-  seek(position) {
-    this.elAudio.currentTime = position
-  }
+	setSpeed(speed) {
+		this.elAudio.playbackRate = speed
+	}
+
+	seek(position) {
+		this.elAudio.currentTime = position
+	}
 }
 
 let instance = null
 export function init(store) {
-  if (instance) throw new Error('AudioPlayer allready instantiated')
-  instance = new AudioPlayer(store)
+	if (instance) throw new Error('AudioPlayer allready instantiated')
+	instance = new AudioPlayer(store)
 }
 export function getInstance() {
-  if (!instance) throw new Error('AudioPlayer is not instantiated')
-  return instance
+	if (!instance) throw new Error('AudioPlayer is not instantiated')
+	return instance
 }
