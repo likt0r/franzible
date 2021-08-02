02eb0a5ee8666f98882904bb943f1eaec4c9f343
bewr <benjamin.werner@mfn.berlin>
2021-08-02 11:35:39 +0200

    fix type issues with mongoose

--- backend/src/declarations.d.ts
+++ backend/src/declarations.d.ts
@@ -1,6 +1,45 @@
-import { Application as ExpressFeathers } from '@feathersjs/express';
+import { Application as ExpressFeathers } from '@feathersjs/express'
+import { Query, Document } from 'mongoose'
 
 // A mapping of service names to types. Will be extended in service files.
 export interface ServiceTypes {}
 // The application instance type that will be used everywhere else
-export type Application = ExpressFeathers<ServiceTypes>;
+export type Application = ExpressFeathers<ServiceTypes>
+
+export type MongooseFuzzySearchingField<T = any> =
+	| keyof T
+	| {
+			name: keyof T
+			minSize?: number
+			weight?: number
+			prefixOnly?: boolean
+			escapeSpecialCharacters?: boolean
+			keys?: (string | number)[]
+	  }
+
+export interface MongooseFuzzySearchingMiddlewares<T = unknown> {
+	preSave(this: Query<T, Document<any>>): void
+	preInsertMany(this: Query<T, Document<any>>): Promise<void>
+	preUpdate(this: Query<T, Document<any>>): Promise<void>
+	preUpdateOne(): Promise<void>
+	preFindOneAndUpdate(this: Query<T, Document<any>>): Promise<void>
+	preUpdateMany(this: Query<T, Document<any>>): Promise<void>
+}
+
+export interface MongooseFuzzySearchingOptions<
+	T extends Record<string, unknown>
+> {
+	fields?: MongooseFuzzySearchingField<T>[]
+	middlewares?: MongooseFuzzySearchingMiddlewares<T>
+}
+
+export interface MongooseFuzzySearchingParams {
+	query: string
+	minSize?: string
+	prefixOnly?: boolean
+	exact?: boolean
+}
+
+export interface Model<T extends Document> {
+	fuzzySearch(query: string | MongooseFuzzySearchingParams): Promise<T[]>
+}
--- backend/src/models/books.model.ts
+++ backend/src/models/books.model.ts
@@ -2,43 +2,41 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-import { Application } from '../declarations'
-import { MongooseFuzzySearchingField } from 'mongoose'
+import { Application, MongooseFuzzySearchingField } from '../declarations'
 import fuzzySearching from 'mongoose-fuzzy-searching'
 import { Model, Mongoose } from 'mongoose'
 
 const fields: MongooseFuzzySearchingField<any>[] = [
-  { name: 'author' },
-  { name: 'title' },
-  'series',
+	{ name: 'author' },
+	{ name: 'title' },
+	'series',
 ]
 
 export default function (app: Application): Model<any> {
-  const modelName = 'books'
-  const mongooseClient: Mongoose = app.get('mongooseClient')
-  const schema = new mongooseClient.Schema(
-    {
-      author: { type: String },
-      title: { type: String },
-      series: [String],
-      files: [{ filepath: String, filename: String, duration: Number }],
-      cover: [String],
-      genre: { type: String },
-      language: { type: String },
-      type: { type: String },
-      mediaPath: {type: String}
+	const modelName = 'books'
+	const mongooseClient: Mongoose = app.get('mongooseClient')
+	const schema = new mongooseClient.Schema(
+		{
+			author: { type: String },
+			title: { type: String },
+			series: [String],
+			files: [{ filepath: String, filename: String, duration: Number }],
+			cover: [String],
+			genre: { type: String },
+			language: { type: String },
+			type: { type: String },
+			mediaPath: { type: String },
+		},
+		{
+			timestamps: true,
+		}
+	)
+	schema.plugin(fuzzySearching, { fields })
 
-    },
-    {
-      timestamps: true,
-    }
-  )
-  schema.plugin(fuzzySearching, { fields })
-
-  // This is necessary to avoid model compilation errors in watch mode
-  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
-  if (mongooseClient.modelNames().includes(modelName)) {
-    mongooseClient.deleteModel(modelName)
-  }
-  return mongooseClient.model<any>(modelName, schema)
+	// This is necessary to avoid model compilation errors in watch mode
+	// see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+	if (mongooseClient.modelNames().includes(modelName)) {
+		mongooseClient.deleteModel(modelName)
+	}
+	return mongooseClient.model<any>(modelName, schema)
 }
