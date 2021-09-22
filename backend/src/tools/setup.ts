63df595190ca9b6afef6fce8d19ad8bc8f397314
bewr <benjamin.werner42@protonmail.com>
2021-09-22 15:56:09 +0200

    fix admin acount creation on intial start

--- backend/src/tools/setup.ts
+++ backend/src/tools/setup.ts
@@ -1,16 +1,21 @@
 import * as mongoose from 'mongoose'
 import { Application } from '../declarations'
 export async function createAdminAccount(app: Application): Promise<void> {
-  const User = await mongoose.model('users')
-  const count = await User.estimatedDocumentCount()
+	const User = await mongoose.model('users')
+	const count = await User.estimatedDocumentCount()
 
-  if (count === 0) {
-    const service = app.service('users')
-    service.create({
-      email: 'admin',
-      password: 'admin',
-      isAdmin: true,
-      strategy: 'local',
-    })
-  }
+	if (count === 0) {
+		const authService = (app as any).defaultAuthentication()
+
+		const [localStrategy] = authService.getStrategies('local')
+
+		const hashedPassword = await (localStrategy as any).hashPassword('admin')
+
+		User.create({
+			email: 'admin',
+			password: hashedPassword,
+			isAdmin: true,
+			strategy: 'local',
+		})
+	}
 }
